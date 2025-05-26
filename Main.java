64de5c704ddc1ffba0e017a4c6c5fddd5f0cb661
Alan369 <cs25001@esfe.agape.edu.sv>
2025-05-26 10:09:34 -0600

    creacion de la matris

--- Main.java
+++ Main.java
@@ -3,6 +3,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
+        int[] numeros = new int[12];
+         int pares = 0, impares = 0;
+         Scanner sc = new Scanner(System.in);
+
+         for (int i = 0; i < numeros.length; i++) {
+             System.out.print("Ingrese el número " + (i + 1) + ": ");
+             numeros[i] = sc.nextInt();
+         }
          
      }
     
